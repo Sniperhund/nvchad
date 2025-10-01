0a608755402e780dfad69667bd6cd8c3b86ede0b
Lucas Täkker <lucastakker@gmail.com>
2025-10-01 09:44:17 +0200

    Updated LSP

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,18 +1,22 @@
 local options = {
     formatters_by_ft = {
-        lua = { "stylua" },
+        lua = false,
         css = { "prettier" },
         scss = { "prettier" },
         html = { "prettier" },
         javascript = { "prettier" },
         typescript = { "prettier" },
         typescriptreact = { "prettier" },
+        vue = { "prettier" },
+
+        c = false,
+        cpp = false,
     },
 
     format_on_save = {
         -- These options will be passed to conform.format()
         timeout_ms = 500,
-        lsp_fallback = true,
+        lsp_fallback = false,
     },
 }
 
--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,8 +1,6 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
-
 local servers = { "html", "clangd", "asm_lsp", "cmake", "pylsp", "somesass_ls", "emmet_language_server", "tailwindcss", "glsl_analyzer", "rust_analyzer" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
@@ -13,24 +11,83 @@ local function custom_on_attach(client, bufnr)
     vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, opts)
 end
 
-for _, lsp in ipairs(servers) do
-    lspconfig[lsp].setup {
-        on_attach = custom_on_attach,
-        on_init = nvlsp.on_init,
-        capabilities = nvlsp.capabilities,
-    }
+local mason_registry = require("mason-registry")
+mason_registry.refresh()
+
+local function ensure_mason(pkg)
+  if not mason_registry.has_package(pkg) then
+    vim.notify("mason: unknown package " .. pkg, vim.log.levels.ERROR)
+    return
+  end
+  local p = mason_registry.get_package(pkg)
+  if not p:is_installed() then
+    vim.notify("Installing " .. pkg .. " via mason …")
+    p:install()
+  end
 end
 
-lspconfig.ts_ls.setup {
-    on_attach = custom_on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-    opts = {
-        preferences = {
-            includeCompletionsForModuleExports = true,
-            includeCompletionsForImportStatements = true,
-            importModuleSpecifierPreference = "relative",
-        }
-    }
-}
+ensure_mason("vue-language-server")
+ensure_mason("typescript-language-server")
+ensure_mason("typescript")
+
+local mason_root = vim.fn.stdpath("data") .. "/mason/packages"
+local vue_ls_path = mason_root
+  .. "/vue-language-server/node_modules/@vue/language-server"
+
+vim.lsp.enable(servers)
+
+vim.lsp.config("ts_ls", {
+    filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+    init_options = {
+        plugins = {
+            {
+                name = "@vue/typescript-plugin",
+                location = vue_ls_path,
+                languages = { "vue" },
+            },
+        },
+    },
+    settings = {
+        typescript = {
+            inlayHints = {
+                includeInlayParameterNameHints = "all",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+            },
+        },
+    },
+})
+vim.lsp.enable("ts_ls")
+
+vim.lsp.config("vue_ls", {
+    filetypes = { "vue" },
+    root_markers = { "vue.config.js", "vue.config.ts", "nuxt.config.js", "nuxt.config.ts" },
+    init_options = {
+        vue = { hybridMode = false },
+        typescript = {
+            tsdk = vim.fn.getcwd() .. "/node_modules/typescript/lib",
+        },
+    },
+    settings = {
+        typescript = {
+            inlayHints = {
+                enumMemberValues = { enabled = true },
+                functionLikeReturnTypes = { enabled = true },
+                propertyDeclarationTypes = { enabled = true },
+                parameterTypes = {
+                    enabled = true,
+                    suppressWhenArgumentMatchesName = true,
+                },
+                variableTypes = { enabled = true },
+            },
+        },
+    },
+})
+vim.lsp.enable("vue_ls")
+
 
